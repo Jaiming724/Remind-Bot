91fc607502e83686e4c8253dd0aac3c879f5505b
Jiaming <Jiamingw29@gmail.com>
2022-08-01 01:26:30 -0400

    Added NotionTime to test

--- src/test/java/NotionTest.kt
+++ src/test/java/NotionTest.kt
@@ -1,5 +1,6 @@
 import dev.scratch.remindbot.NotionHelper
 import dev.scratch.remindbot.Task
+import dev.scratch.remindbot.util.NotionTime
 import notion.api.v1.NotionClient
 import notion.api.v1.http.OkHttp4Client
 import org.junit.jupiter.api.Assertions.assertFalse
@@ -14,11 +15,10 @@ class NotionTest {
     fun taskTests() {
         val client = NotionClient(System.getenv("notion"));
         client.httpClient = OkHttp4Client()
-
         val timeOffset: String = (ZoneOffset.systemDefault().rules.getOffset(Instant.now()).toString())
         assertEquals(timeOffset, "-04:00")
         val notionTest = NotionHelper(client)
-        val task = Task("Testing Task", "2022-06-09T20:34:00.000-04:00", "2022-06-09", false, true)
+        val task = Task("Testing Task", NotionTime("2022-06-09T20:34:00.000-04:00"), "2022-06-09", false, true)
         task.id = notionTest.addTask(task)
         var tasks = notionTest.getTasks()
         assertTrue(task in tasks)
@@ -35,5 +35,5 @@ class NotionTest {
 
         assertFalse(task in tasks)
     }
-
+    
 }
